53dbe0b56c7e24d078749f185b736103c1129449
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-02-27 12:26:25 +0000

    Automatically backed up by Learn

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ group :development, :test do
   gem 'pry'
   gem 'capybara'
   gem 'rspec-rails'
+  gem "pry"
 end
 
 group :development do
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    
+      
   end
 
   def artist_name=(name)
+     
   end
 end
